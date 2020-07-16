5bcfcab034ce2db6999aeecbae1ec943d7550c90
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2020-07-16 19:25:59 +0100

    tiny UI tweaks

--- src/Heading.js
+++ src/Heading.js
@@ -3,12 +3,12 @@ import React from "react";
 const Heading = () => {
   return (
     <header className="App-header">
-      CYF Hotel &nbsp;
       <img
         className="App-header-logo"
         src="https://image.flaticon.com/icons/svg/139/139899.svg"
         alt="hotel icon"
       />
+      &nbsp; CYF Hotel
     </header>
   );
 };
